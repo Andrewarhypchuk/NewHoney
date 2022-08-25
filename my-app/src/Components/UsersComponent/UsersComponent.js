95bce802bf7fcbf0201e3fe161b1a6cfb48f23f5
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2022-08-25 10:14:58 +0300

    ...

--- my-app/src/Components/UsersComponent/UsersComponent.js
+++ my-app/src/Components/UsersComponent/UsersComponent.js
@@ -1,13 +1,24 @@
-import React from "react";
-import { useSelector } from "react-redux";
+import React ,{useEffect} from "react";
+import { useSelector,useDispatch } from "react-redux";
 import TranslateComponent from "../TranslateComponent/TranslateComponent";
 import { selectAdmin } from "./../../Redux/userSettings-reducer";
+import { selectUsers, setUsers } from './../../Redux/users-reducer';
 
 const Users = () => {
-  let isAdmin = useSelector(selectAdmin);
+  const dispatch = useDispatch();
+  const isAdmin = useSelector(selectAdmin);
+  const users = useSelector(selectUsers);
+ 
+  const usersPresentation = users.users.map((user)=>{
+           return <div>{user.id}</div>
+  })
+
+  useEffect(()=>{
+    users.users.length === 0 && dispatch(setUsers);
+  })
 
   return isAdmin ? (
-    <TranslateComponent str="Users" />
+    {usersPresentation}
   ) : (
     <TranslateComponent str="You dont have access" />
   );
