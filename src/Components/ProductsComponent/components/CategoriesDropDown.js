9cccc60b330efc9fc33281d9c3eeca38694db4d2
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2022-09-23 12:04:26 +0300

    updated filter

--- src/Components/ProductsComponent/components/CategoriesDropDown.js
+++ src/Components/ProductsComponent/components/CategoriesDropDown.js
@@ -1,18 +1,17 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { useDispatch } from 'react-redux';
 
 import Dropdown from "react-bootstrap/Dropdown";
 import TranslateComponent from './../../TranslateComponent/TranslateComponent';
 import { ALL, ELECTRONICS, JEWELERY, MEN_CLOTHING, WOMEN_CLOTHING } from './../../../Utiles/consts';
 import { changeCategory } from "../../../Redux/products-reducer";
-import { eventListeners } from "@popperjs/core";
+
 
 const CategoriesList = () => {
     const dispatch = useDispatch();
-    const [activeItem,setActiveItem] = useState(ALL)
-    
-    const handleSelectedChange = (eventKey,event) =>{
-        console.log(eventKey)
+    const [activeItem, setActiveItem] = useState(ALL)
+
+    const handleSelectedChange = (eventKey, event) => {
         setActiveItem(eventKey)
         dispatch(changeCategory(eventKey))
     }
@@ -24,26 +23,31 @@ const CategoriesList = () => {
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item
+                    active={activeItem === ALL}
                     eventKey={ALL}
                 >
                     <TranslateComponent str={ALL} />
                 </Dropdown.Item>
                 <Dropdown.Item
+                    active={activeItem === ELECTRONICS}
                     eventKey={ELECTRONICS}
                 >
                     <TranslateComponent str={ELECTRONICS} />
                 </Dropdown.Item>
                 <Dropdown.Item
+                    active={activeItem === JEWELERY}
                     eventKey={JEWELERY}
                 >
                     <TranslateComponent str={JEWELERY} />
                 </Dropdown.Item>
                 <Dropdown.Item
+                    active={activeItem === MEN_CLOTHING}
                     eventKey={MEN_CLOTHING}
                 >
                     <TranslateComponent str={MEN_CLOTHING} />
                 </Dropdown.Item>
                 <Dropdown.Item
+                    active={activeItem === WOMEN_CLOTHING}
                     eventKey={WOMEN_CLOTHING}
                 >
                     <TranslateComponent str={WOMEN_CLOTHING} />
