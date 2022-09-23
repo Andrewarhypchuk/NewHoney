8a9eeb1e1c263a134456c1bd2947ab271a503f90
Andrewarhypchuk <andrewarhypchuk@gmail.com>
2022-09-23 11:59:06 +0300

    ...

--- src/Components/ProductsComponent/components/CategoriesDropDown.js
+++ src/Components/ProductsComponent/components/CategoriesDropDown.js
@@ -1,47 +1,50 @@
-import React from "react";
+import React,{useState} from "react";
 import { useDispatch } from 'react-redux';
 
 import Dropdown from "react-bootstrap/Dropdown";
 import TranslateComponent from './../../TranslateComponent/TranslateComponent';
 import { ALL, ELECTRONICS, JEWELERY, MEN_CLOTHING, WOMEN_CLOTHING } from './../../../Utiles/consts';
 import { changeCategory } from "../../../Redux/products-reducer";
+import { eventListeners } from "@popperjs/core";
 
 const CategoriesList = () => {
     const dispatch = useDispatch();
+    const [activeItem,setActiveItem] = useState(ALL)
+    
+    const handleSelectedChange = (eventKey,event) =>{
+        console.log(eventKey)
+        setActiveItem(eventKey)
+        dispatch(changeCategory(eventKey))
+    }
 
     return (
-        <Dropdown>
+        <Dropdown onSelect={handleSelectedChange}>
             <Dropdown.Toggle className="buttonLanguage languageButton" id="dropdown-basic">
                 <TranslateComponent str="Filter by category" />
             </Dropdown.Toggle>
             <Dropdown.Menu>
                 <Dropdown.Item
-                    onClick={() => dispatch(changeCategory(ALL))}
-                    href="#/action-1"
+                    eventKey={ALL}
                 >
                     <TranslateComponent str={ALL} />
                 </Dropdown.Item>
                 <Dropdown.Item
-                    onClick={() => dispatch(changeCategory(ELECTRONICS))}
-                    href="#/action-1"
+                    eventKey={ELECTRONICS}
                 >
                     <TranslateComponent str={ELECTRONICS} />
                 </Dropdown.Item>
                 <Dropdown.Item
-                    onClick={() => dispatch(changeCategory(JEWELERY))}
-                    href="#/action-2"
+                    eventKey={JEWELERY}
                 >
                     <TranslateComponent str={JEWELERY} />
                 </Dropdown.Item>
                 <Dropdown.Item
-                    onClick={() => dispatch(changeCategory(MEN_CLOTHING))}
-                    href="#/action-2"
+                    eventKey={MEN_CLOTHING}
                 >
                     <TranslateComponent str={MEN_CLOTHING} />
                 </Dropdown.Item>
                 <Dropdown.Item
-                    onClick={() => dispatch(changeCategory(WOMEN_CLOTHING))}
-                    href="#/action-2"
+                    eventKey={WOMEN_CLOTHING}
                 >
                     <TranslateComponent str={WOMEN_CLOTHING} />
                 </Dropdown.Item>
